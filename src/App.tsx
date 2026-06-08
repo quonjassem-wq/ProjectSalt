7db1006ba75690aa97c495557285187937c52ecb
quonjassem-wq <quonjassem@gmail.com>
2026-06-08 20:35:24 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Your Real App Here</h1>
+      {/* Import and use your web/ or mobile/ components */}
+    </div>
+  );
+}
+
+export default App;
